df0681a1aeb9311301496f305686879f3b639111
adelmar526 <adelmar526@gmail.com>
2022-11-15 17:06:43 +0000

    Product.js

--- web-mobile/Product.js
+++ web-mobile/Product.js
@@ -1,13 +1,91 @@
-let products = [
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}
-
+let products = [{
+        id: 1001,
+        title: "Japanese Lesson",
+        description: "Japanese lessons tought by <em>professional</em> Japanese speakers!",
+        price: "£20",
+        loc: "South London",
+        spaces: 5,
+        image: "assets/japanese.png"
+    },
+    {
+        id: 1002,
+        title: "French Lesson",
+        description: "French lessons tought by <em>professional</em> French speakers!",
+        price: "£10",
+        loc: "Central London",
+        spaces: 5,
+        image: "assets/japanese.png"
+    },
+    {
+        id: 1003,
+        title: "Portuguese Lesson",
+        description: "Portuguese lessons tought by <em>professional</em> Portuguese speakers!",
+        price: "£15",
+        loc: "East London",
+        spaces: 5,
+        image: "assets/japanese.png"
+    },
+    {
+        id: 1004,
+        title: "English Lesson",
+        description: "English lessons tought by <em>professional</em> English speakers!",
+        price: "£5",
+        loc: "North London",
+        spaces: 5,
+        image: "assets/japanese.png"
+    },
+    {
+        id: 1005,
+        title: "Spanish Lesson",
+        description: "Spanish lessons tought by <em>professional</em> Spanish speakers!",
+        price: "£15",
+        loc: "East London",
+        spaces: 5,
+        image: "assets/japanese.png"
+    },
+    {
+        id: 1006,
+        title: "German Lesson",
+        description: "German lessons tought by <em>professional</em> German speakers!",
+        price: "£25",
+        loc: "West London",
+        spaces: 5,
+        image: "assets/japanese.png"
+    },
+    {
+        id: 1007,
+        title: "Mandarin Lessons",
+        description: "Mandarin lessons tought by <em>professional</em> Mandarin speakers!",
+        price: "£20",
+        loc: "Noth London",
+        spaces: 5,
+        image: "assets/japanese.png"
+    },
+    {
+        id: 1008,
+        title: "Swahili Lesson",
+        description: "Swahili lessons tought by <em>professional</em> Swahili speakers!",
+        price: "£30",
+        loc: "North London",
+        spaces: 5,
+        image: "assets/japanese.png"
+    },
+    {
+        id: 1009,
+        title: "Russian Lesson",
+        description: "Russian lessons tought by <em>professional</em> Russian speakers!",
+        price: "£25",
+        loc: "South London",
+        spaces: 5,
+        image: "assets/japanese.png"
+    },
+    {
+        id: 1010,
+        title: "Arabic Lesson",
+        description: "Arabic lessons tought by <em>professional</em> Arabic speakers!",
+        price: "£20",
+        loc: "East London",
+        spaces: 5,
+        image: "assets/japanese.png"
+    },
 ]
